55952bc45dadaa7b8b4527251867f310fb61145a
Daijiro Wachi <daijiro.wachi@gmail.com>
2013-01-27 19:19:17 +0900

    new file:   watilde.js

---
+++ watilde.js
@@ -0,0 +1,32 @@
+(function(){
+	var Prime, stdin, stdout;
+	Prime = (function () {
+		function Prime(stdin) {
+			this.stdin = stdin;
+		}
+		Prime.prototype.is = function (n) {
+			if (n < 2) { return false; }
+			if (n === 2) { return true; }
+			if (n % 2 === 0) { return false; }
+			var i = 3;
+			for (i; i * i <= n; i += 2) {
+				if (n % i === 0) { return false; }
+			}
+			return true;
+		};
+		Prime.prototype.calc = function () {
+			var i = 1, count = 1, stdout = 0;
+			for (i; this.stdin >= count; i += 1) {
+				if (this.is(i)) {
+					count += 1;
+					stdout += i;
+				}
+			}
+			return stdout;
+		};
+		return Prime;
+	}());
+	stdin = 10000;
+	stdout = new Prime(stdin).calc();
+	console.log(stdout)
+}());
