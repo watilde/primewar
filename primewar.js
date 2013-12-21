5ebebd7412c9826b9072f3310ff8873ea446f13a
Daijiro Wachi <daijiro.wachi@gmail.com>
2013-12-21 19:05:47 +0900

    Fixes function style and add use strict

--- primewar.js
+++ primewar.js
@@ -1,30 +1,29 @@
-var Prime, stdin, stdout;
-Prime = (function () {
-  function Prime(stdin) {
-    this.stdin = stdin;
+'use strict';
+var stdin, stdout;
+function Prime(stdin) {
+  this.stdin = stdin;
+}
+Prime.prototype.is = function (n) {
+  if (n < 2) { return false; }
+  if (n === 2) { return true; }
+  if (n % 2 === 0) { return false; }
+  var i = 3;
+  for (i; i * i <= n; i += 2) {
+    if (n % i === 0) { return false; }
   }
-  Prime.prototype.is = function (n) {
-    if (n < 2) { return false; }
-    if (n === 2) { return true; }
-    if (n % 2 === 0) { return false; }
-    var i = 3;
-    for (i; i * i <= n; i += 2) {
-      if (n % i === 0) { return false; }
+  return true;
+};
+Prime.prototype.calc = function () {
+  var i = 1, count = 1, stdout = 0;
+  for (i; this.stdin >= count; i += 1) {
+    if (this.is(i)) {
+      count += 1;
+      stdout += i;
     }
-    return true;
-  };
-  Prime.prototype.calc = function () {
-    var i = 1, count = 1, stdout = 0;
-    for (i; this.stdin >= count; i += 1) {
-      if (this.is(i)) {
-        count += 1;
-        stdout += i;
-      }
-    }
-    return stdout;
-  };
-  return Prime;
-}());
+  }
+  return stdout;
+};
+
 stdin = 10000;
 stdout = new Prime(stdin).calc();
 console.log(stdout)
